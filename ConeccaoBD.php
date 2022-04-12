52df9c2098326c48dbcc006e9f797f4b87a5881b
Denilson de Oliveira Carneiro <86687247+Denils0n@users.noreply.github.com>
2022-04-12 07:35:33 -0300

    Bando pra teste e seu arquivo de conexão

---
+++ ConeccaoBD.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    $localhost = "localhost" ;
+    $usuario = 'admin';
+    $senha = 'admin';
+    $banco = "VEGANOLOGO" ;
+    global  $pdo ;
+
+    try {
+        $pdo = new  PDO ( "mysql:dbname=" .$banco. "; host =" .$localhost , $usuario , $senha );
+        $pdo -> setAttribute ( PDO :: ATTR_ERRMODE , PDO :: ERRMODE_EXCEPTION );
+    } catch ( Exception  $e ) {
+        echo  "ERRO:" .$e->getMessage();	
+        exit();
+        
+    }
+?>
